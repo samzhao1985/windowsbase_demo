92f5410db20c02a46e9d39090bd3959aa2f40c4e
Sam <sam@localhost.(none)>
2011-06-17 01:34:46 +0800

    Initial Commit

---
+++ windowsbase_demo/windowsbase_demoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  windowsbase_demoAppDelegate.h
+//  windowsbase_demo
+//
+//  Created by Sam on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface windowsbase_demoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
